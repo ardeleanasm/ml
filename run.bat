a81b95c304686330d55d14370459e50f9c465c5b
ars <ardeleanasm@gmail.com>
2023-04-25 17:48:01 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-docker run -p 8888:8888 -it -v c:\Users\sardelean\Documents\Docker\python\work:/app --entrypoint /bin/bash jupyter
+docker run -p 8888:8888 -it -v .\work:/app --entrypoint /bin/bash jupyter
